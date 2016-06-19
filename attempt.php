17c3b61fba1cc2910ed42e1f9732763be70387e3
scottclee <scott@clee.pro>
2016-06-19 13:08:19 +0100

    Add origin IP logging in webservice and create clean up script for log

--- attempt.php
+++ attempt.php
@@ -1,17 +1,19 @@
 <?php
 
-// get the HTTP method, path and body of the request
+// Get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
 $pathInfo = $_SERVER['PATH_INFO'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $jsonInput = file_get_contents('php://input');
 $input = json_decode(file_get_contents('php://input'),true);
 
+// NOTE: at the moment this is storing the IP address of kit-encrypt proxy - what can be done about this?
+$input['remoteAddr'] = $remoteAddr;
+$input['httpXForwardedFor'] = $httpXForwardedFor
+
 $connection = new MongoClient("mongodb://localhost:27017");
 $dbname = $connection->selectDB('skad');
 $attempts = $dbname->attempts;
 $attempts->insert($input);
-
-//TODO Also need to get the ip address of the calling client
 
 ?>
