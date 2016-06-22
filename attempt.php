98c2d8a110688b8a08bef88427e76e9b9c263fe8
scottclee <scott.john.clee@gmail.com>
2016-06-22 09:46:47 +0100

    Fix tweeting from SKAD Dog

--- attempt.php
+++ attempt.php
@@ -40,19 +40,48 @@ switch ($method) {
 	$sourceJson = file_get_contents("http://ip-api.com/json/".$ipaddress);
 	$source = json_decode($sourceJson, true);
 
+	// Temporary fix
+	$device = "";
+
+	if ($input['key'] === 'b8:27:eb:ee:5f:ee') {
+		$device = "Rocky";
+	}
+	else if ($input['key'] === 'b8:27:eb:7c:5e:37') {
+		$device = "Didier";
+	}
+
 	if ($source['message'] !== 'invalid query') {
-		$twitterMessage = $source['org']." ($remoteAddr) tried to logon as [".$input['user']."] from ".$source['city'];
+		file_put_contents($debugfile, "############################\n", FILE_APPEND | LOCK_EX);
 		
+		$tweet = $source['org']." ($ipaddress) tried to logon as [".$input['user']."] from ".$source['city']." in ".$source['city']. " #alerted =".$device."=";
+
+		$shellExec = "/usr/bin/sudo ";
+		$twitterExec = $shellExec."/usr/local/bin/t";
+
 		if ($input['user'] === 'skadtest') {
-			file_put_contents($debugfile, $twitterMessage."\n", FILE_APPEND | LOCK_EX);			
+			file_put_contents($debugfile, "Switching twitter account to SKAD_Test\n", FILE_APPEND | LOCK_EX);
+			$command = $twitterExec." set active SKAD_Test tmv9q0pmITaLbNZG2PkeH80t1";
+			file_put_contents($debugfile, "command=".$command."\n", FILE_APPEND | LOCK_EX);			
+			$output = shell_exec($command." 2>&1");
+			file_put_contents($debugfile, "shell_exec output=".$output."\n", FILE_APPEND | LOCK_EX);		
 		}
-		else {
-			file_put_contents($debugfile, "About to tweet -> ".$twitterMessage."\n", FILE_APPEND | LOCK_EX);			
-			shell_exec("/home/pi/php_root /usr/local/bin/t update '".$twitterMessage."'");
+		
+		$command = $twitterExec." update \"".$tweet."\"";
+		
+		file_put_contents($debugfile, "About to tweet -> ".$tweet."\n", FILE_APPEND | LOCK_EX);			
+		file_put_contents($debugfile, "command=".$command."\n", FILE_APPEND | LOCK_EX);			
+		$output = shell_exec($command." 2>&1");
+		file_put_contents($debugfile, "shell_exec output=".$output."\n", FILE_APPEND | LOCK_EX);			
+		if ($input['user'] === 'skadtest') {
+			file_put_contents($debugfile, "Switching twitter account back to SKAD_Dog\n", FILE_APPEND | LOCK_EX);
+			$command = $twitterExec." set active SKAD_Dog 8FLtRR906YYAFq7wmFaYCmDMA";
+			file_put_contents($debugfile, "command=".$command."\n", FILE_APPEND | LOCK_EX);			
+			$output = shell_exec($command." 2>&1");
+			file_put_contents($debugfile, "shell_exec output=".$output."\n", FILE_APPEND | LOCK_EX);	
 		}
 	}
 	else {
-		file_put_contents($debugfile, "Login made from local network: ".$ipaddress."\n", FILE_APPEND | LOCK_EX);			
+		file_put_contents($debugfile, "Login made from local network: ".$ipaddress."\n", FILE_APPEND | LOCK_EX);
 	}
 
         break;
