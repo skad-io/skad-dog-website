d93adcee6ea89b66a8a77b60ece7a14394755522
katshann <kat@demigoth.org>
2016-07-23 12:40:44 +0100

    Structure for styling

---
+++ template.php
@@ -0,0 +1,208 @@
+<!doctype html>
+<html lang="en">
+<?php include 'head.html'; ?> 
+<body class="flat">
+<?php include 'nav.html'; ?> 
+
+       
+                    
+            <section class="cover fullscreen image-slider slider-all-controls controls-inside parallax">
+                <ul class="slides">
+                    <li class="overlay image-bg">
+                        <div class="background-image-holder">
+                            <img alt="image" class="background-image" src="img/home.png">
+                        </div>
+                        <div class="container v-align-transform">
+                            <div class="row">
+                                <div class="col-md-6 col-sm-8">
+                                    <h1 class="mb40 mb-xs-16 large">Secure your shit</h1>
+                                    <h6 class="uppercase mb16">Your fridge is funding terrorists</h6>
+                                    <p class="lead mb40">
+                                        Know who is knocking on your door
+                                        <br>and why.
+                                    </p>
+                                    <a class="btn btn-lg" href="#">Start Exploring</a>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        
+                    </li>
+                </ul>
+            </section><section class="page-title page-title-3 bg-secondary">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 text-center">
+                            <h3 class="uppercase mb0">A text section</h3>
+                            <p>Something interesting</p>
+                        </div>
+                    </div>
+                    
+                </div>
+            </section><section class="page-title page-title-3 image-bg overlay parallax">
+                <div class="background-image-holder">
+                    <img alt="Infographic" class="background-image" src="img/infographic.png">
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 text-center">
+                            <h3 class="uppercase mb0">Image with text over. Maybe infographic?</h3>
+                        </div>
+                    </div>
+                    
+                </div>
+           </section><section class="image-edge">
+                <div class="col-md-6 col-sm-4 p0">
+                    <img alt="Screenshot" class="mb-xs-24" src="img/graphs.png">
+                </div>
+                <div class="container">
+                    <div class="col-md-5 col-md-offset-1 col-sm-7 col-sm-offset-1 v-align-transform right">
+                        <h1 class="large mb40 mb-xs-16">Graph</h1>
+                        <h6 class="uppercase mb16">Where are the attacks coming from?</h6>
+                        <p class="lead mb40">
+                            We've graphed the attacks made on our test boxes out on the internet.
+                        </p>
+                    </div>
+                </div>
+                
+            </section>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-4 text-center">
+                            <div class="feature">
+                                <i class="ti-gallery icon fade-3-4 inline-block mb16"></i>
+                                <h4>Scrolly of products</h4>
+                                <p>
+                                    But we only have one at the moment.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 text-center">
+                            <div class="feature">
+                                <i class="ti-package icon fade-3-4 inline-block mb16"></i>
+                                <h4 class="AlbiaNova">Defender Dog</h4>
+                                <p>
+                                    
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 text-center">
+                            <div class="feature">
+                                <i class="ti-layers icon fade-3-4 inline-block mb16"></i>
+                                <h4 class="AlbiaNova">Mr Postman</h4>
+                                <p>
+                                    Coming soon...
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                
+            </section><section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 text-center">
+                            <h4 class="uppercase mb16">Who are these fuckers?</h4>
+                            <p class="lead mb80">
+                                SKAD is made of awesome. 
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                       <div class="col-md-8 col-sm-10 col-sm-offset-1 col-md-offset-2">
+                            <div class="horizontal-tile vpe">
+                                <div class="tile-left">
+                                    <a href="#">
+                                        <div class="background-image-holder">
+                                            <img alt="image" class="background-image" src="img/kat.png">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="tile-right bg-secondary">
+                                    <div class="description">
+                                        <h4 class="mb8">Kat Shann</h4>
+                                        <h6 class="uppercase">
+                                            Graphics
+                                        </h6>
+                                        <p>
+                                            She draws 2D shit.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="horizontal-tile vpe">
+                                <div class="tile-left">
+                                    <a href="#">
+                                        <div class="background-image-holder">
+                                            <img alt="image" class="background-image" src="img/dan.png">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="tile-right bg-secondary">
+                                    <div class="description">
+                                        <h4 class="mb8">Daniel Would</h4>
+                                        <h6 class="uppercase">
+                                            Hardware
+                                        </h6>
+                                        <p>
+                                            He draws 3D shit. 
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+<div class="horizontal-tile vpe">
+                                <div class="tile-left">
+                                    <a href="#">
+                                        <div class="background-image-holder">
+                                            <img alt="image" class="background-image" src="img/ana.png">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="tile-right bg-secondary">
+                                    <div class="description">
+                                        <h4 class="mb8">Ana Semenova</h4>
+                                        <h6 class="uppercase">
+                                            Strategy
+                                        </h6>
+                                        <p>
+                                            She talks to real humans. 
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+<div class="horizontal-tile vpe">
+                                <div class="tile-left">
+                                    <a href="#">
+                                        <div class="background-image-holder">
+                                            <img alt="image" class="background-image" src="img/scott.png">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="tile-right bg-secondary">
+                                    <div class="description">
+                                        <h4 class="mb8">Scott Clee</h4>
+                                        <h6 class="uppercase">
+                                            Software
+                                        </h6>
+                                        <p>
+                                            He writes code. 
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                       </div>
+                    </div>
+                </div>
+            </section>
+
+
+
+
+    <?php include 'footer.html'; ?> 
+    </body>
+</html>
